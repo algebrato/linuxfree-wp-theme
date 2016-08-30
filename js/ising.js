7898fca414e48f08e5516cdd4c84c5e525d66eeb
Stefano Mandelli <linuxfree2@gmail.com>
2016-08-30 22:18:09 +0200

    Update terra.js and fix neighborhood type -> vonNeumann
    Now the neighbors length is always 4 for every site.
    Moore neighborhood is an incorrect aprroximation.

--- js/ising.js
+++ js/ising.js
@@ -4,61 +4,53 @@ var dimension=larg/size;
 if( dimension > 3 ){
 	dimension=3;
 }
-console.log(dimension);
-console.log(Math.floor(dimension));
+var IsingModel = new terra.Terrarium(size, size, {
+  id: 'IsingTerrarium',
+  periodic: true,
+  neighborhood: 'vonNeumann',
+  cellSize: dimension,
+  trails: 0,
+  insertAfter: document.getElementById('isingDIV'),
+  background: 'transparent'
+});
+
+
+
+
 var temp=0.05;
-console.log(Math.floor(dimension))
-var IsingModel =new terra.Terrarium(
-		size,size,
-		"IsingModel",
-		dimension.toFixed(2),
-		document.getElementById('isingDIV'),
-		true,
-		'vonNeumann',
-		0,
-		'transparent'
-		);
-  
 var magnetization = 0;
 var counter=0;
-terra.registerCA({  
+
+terra.registerCA({
+
   type: 'Ising',    
   colorFn: function ()  
   {
 	return this.state==1 ? '255,255,255,255' : '0,0,0,255';
   },
-  process: function (neighbors) 
+  process: function (neighbors, x, y) 
   {
 	counter+=1;
 	if (counter==size*size)
 	{ 
 		counter=magnetization=0;
 	}
+	magnetization +=this.state;
 	if(Math.random() > 0.9 )
 	{	
-		magnetization +=this.state/(size*size);
 		deltaE = 0;
 		for (var i = 0; i < neighbors.length; i++)
-		{ 
 			deltaE += neighbors[i].creature.state;
-		}
-		if (neighbors.length<8)
-		{
-			deltaE *= 8/neighbors.length;
-		}
-		deltaE *= this.state;  
+		deltaE *=this.state;  
 		if (deltaE<0)
-		{
 			this.state *= -1;  
-		}
+		
 		else if (Math.random() < Math.min(1.0,Math.exp(-deltaE*0.5/temp)) )
-		{ 
 			this.state *= -1; 
-		}
 	}
+	return true;
   }
 },function (){
-	this.alive=true;
 	if(Math.random() < 0.5 ){
 		this.state = 1;
 	}else{
